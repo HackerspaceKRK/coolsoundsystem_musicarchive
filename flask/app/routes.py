c0331245d3326e89288084862b8c1b80bfba88ba
Wiktor Przybylski <wikprzybylski@gmail.com>
2019-02-11 01:35:54 +0100

    more compacting to subtemplates, also minor front tweaks, also blacklist

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -3,24 +3,19 @@ from flask import render_template
 from app import db
 from app.models import Song, Play
 
+# clumsy import
+from datetime import datetime
+
+# more clumsiness with global variables ahead
+idblacklist=[None, 'spotify:music:content']
+
+# even clumsier function to pass to jinja template
+def getnormaltime(t):
+    return datetime.fromtimestamp(int(float(t)))
+
 @app.route('/')
 @app.route('/index')
 def index():
-    s = Song.query.all()
-    p = Play.query.all()
+    p = Play.query.order_by(Play.timefrom.desc()).all()
 
-    print(s)
-    songs = [
-        {
-        'artist': 'Dubioza kolektiv',
-        'songname': 'Himna generacije',
-        'album': 'Pjesmice za djecu i odrasle',
-        'id':'2mZYQC01utmvwynQZVvplL',
-        'address': 'https://open.spotify.com/track/2mZYQC01utmvwynQZVvplL',
-        'thumb': 'https://i.scdn.co/image/762af66d1a31d1d68221e3721116ae71a6a339e4',
-        'image': 'https://i.scdn.co/image/8c54f79bbed8ac4af7a270786888c26bca930c42',
-        'timefrom':'13:37',
-        'timeto': '21:37'
-        }
-    ]
-    return render_template('index.html', plays=p)
+    return render_template('index.html', plays=p, bl=idblacklist, tf=getnormaltime)
