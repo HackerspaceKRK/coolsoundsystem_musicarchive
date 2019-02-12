9832d0e51486a76c049902b4f6fc6f619aa923fa
Wiktor Przybylski <wikprzybylski@gmail.com>
2019-02-12 22:22:57 +0100

    more real pagination limit

--- flask/config.py
+++ flask/config.py
@@ -6,4 +6,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, '../shared/app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    PLAYS_ON_INDEX_PER_PAGE = 3
+    PLAYS_ON_INDEX_PER_PAGE = 15
